99c622efe11bc89c881d4133060f526847096d6a
Paula Ortiz Menne <paulaortizmenne@gmail.com>
2022-04-05 19:09:54 -0300

    second update

--- familiares/settings.py
+++ familiares/settings.py
@@ -76,9 +76,13 @@ WSGI_APPLICATION = 'familiares.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'dbfamiliares',
+        'USER': 'postgres',
+        'PASSWORD': 'blanqui31',
+        'HOST': '127.0.0.1',
+        'DATABASE_PORT': '5432',
+        }
 }
 
 
